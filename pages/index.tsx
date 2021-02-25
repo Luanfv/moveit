682be867e1e4a9c0a37bb6f505cf4a877e60aa58
Luan <luanfrancavieira@gmail.com>
2021-02-25 19:57:17 -0300

    start countdown

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,35 @@
 import Head from 'next/head';
+
+import { CompletedChallenges } from '../src/components/CompletedChallenges';
+import { Countdown } from '../src/components/Countdown';
 import { ExporienceBar } from "../src/components/ExperienceBar";
+import { Profile } from '../src/components/Profile';
+
+import styles from '../src/styles/components/Home.module.css';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+        <link rel="shortcut icon" href="favicon.png" type="image/png" />
+        <title>Início | move.it</title>
       </Head>
 
       <ExporienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+
+        <div>
+
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,6 +1,8 @@
+import styles from '../styles/components/ExperienceBar.module.css';
+
 export function ExporienceBar() {
   return (
-    <header className="experience-bar">
+    <header className={styles.experienceBar}>
       <span>
         0 xp
       </span>
@@ -8,7 +10,7 @@ export function ExporienceBar() {
       <div>
         <div style={{ width: '50%' }} />
 
-        <span className="current-experience" style={{ left: '50%' }}>
+        <span className={styles.currentExperience} style={{ left: '50%' }}>
           300 xp
         </span>
       </div>
